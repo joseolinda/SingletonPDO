97828b6b739b56d179899641f964c58ce6fce904
José Olinda <joseolindad@yahoo.com.br>
2019-09-03 18:44:54 -0300

    Autoload clear

--- App/autoload.php
+++ App/autoload.php
@@ -6,8 +6,3 @@ namespace App;
 spl_autoload_register(function ($class){
     require_once str_replace( '\\', '/', $class . ".php");
 });
-
-// require_once 'Routes/web.php';
-//require_once 'Bootstrap.php';
-//
-//new Bootstrap("ClienteController", "Listar");
